397877c3b097ad00a0833de63033632b1705c4c5
James Doyley <jdoyley@gmail.com>
2015-06-29 15:20:44 +0100

    fix again

--- lib/generators/optimadmin/admin/templates/controller.rb
+++ lib/generators/optimadmin/admin/templates/controller.rb
@@ -3,7 +3,7 @@ module Optimadmin
     before_action :set_<%= singular_table_name %>, only: [:show, :edit, :update, :destroy]
 
     def index
-      @<%= plural_table_name %> = <%= class_name %>.all
+      @<%= plural_table_name %> = <%= class_name %>.page(params[:page]).per(15)
     end
 
     def show
