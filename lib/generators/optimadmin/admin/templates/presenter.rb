f0d0b5a36caab9c296a782bdcdda527df109116d
Paul <pauloptimised@users.noreply.github.com>
2016-02-22 12:02:45 +0000

    Amend presenter generation

--- lib/generators/optimadmin/admin/templates/presenter.rb
+++ lib/generators/optimadmin/admin/templates/presenter.rb
@@ -1,18 +1,4 @@
-module Optimadmin
-  class <%= class_name %>Presenter < Optimadmin::BasePresenter
-    presents :<%= singular_table_name %>
-    delegate :id, to: :<%= singular_table_name %>
-
-    def title
-      #<%= singular_table_name %>.title
-    end
-
-    def toggle_title
-      inline_detail_toggle_link(title)
-    end
-
-    def optimadmin_summary
-      #h.simple_format <%= singular_table_name %>.summary
-    end
-  end
+class <%= class_name %>Presenter < BasePresenter
+  presents :<%= singular_table_name %>
+  delegate :id, to: :<%= singular_table_name %>
 end
