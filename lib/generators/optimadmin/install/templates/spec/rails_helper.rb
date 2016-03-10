46ffea949afce3de3a3f5c1aaa5a0394c03dc701
Paul <pauloptimised@users.noreply.github.com>
2016-03-10 12:47:29 +0000

    Amend spec folder

--- lib/generators/optimadmin/install/templates/spec/rails_helper.rb
+++ lib/generators/optimadmin/install/templates/spec/rails_helper.rb
@@ -2,12 +2,12 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'capybara/rspec'
-require 'capybara/poltergeist'
+# require 'capybara/poltergeist'
 require 'shoulda-matchers'
 require 'database_cleaner'
 
